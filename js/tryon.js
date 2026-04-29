419ceec1ddb57c6085782e64cecaae2dea3e7c42
omar-ahmed461 <ohanafy28@gmail.com>
2026-04-29 12:56:26 +0300

    Update tryon.js

--- js/tryon.js
+++ js/tryon.js
@@ -1,7 +1,6 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.158/build/three.module.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.158/examples/jsm/loaders/GLTFLoader.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.158/examples/jsm/controls/OrbitControls.js";
-
+import * as THREE from "https://unpkg.com/three@0.158/build/three.module.js";
+import { GLTFLoader } from "https://unpkg.com/three@0.158/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "https://unpkg.com/three@0.158/examples/jsm/controls/OrbitControls.js";
 let model;
 
 // Scene
